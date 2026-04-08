3844f78ed75b001516826f7988d1c5ee0237db01
taxiLotus <spacepenguin2323@gmail.com>
2026-04-08 09:17:34 -0700

    fixed random unmounting
    sshfs.nvim was automatically unmounting on file exit . ...
    even tho it didnt initialize the mount

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,11 +6,5 @@
 ---@module 'lazy'
 ---@type LazySpec
 return {
-  {
-    'uhs-robert/sshfs.nvim',
-    opts = {
-      -- Refer to the configuration section below
-      -- or leave empty for defaults
-    },
-  },
+  {},
 }
